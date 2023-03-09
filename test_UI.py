6e7c1b7e2206f9778d6a7c027bac264078cfb126
ashfaqq144 <ashfaqq144@gmail.com>
2023-03-09 11:00:25 +0500

    test

--- test_UI.py
+++ test_UI.py
@@ -3,4 +3,4 @@ import time
 from selenium import webdriver
 driver = webdriver.Chrome()
 driver.get("https://www.fafadiatech.com/resources/technical/python/pytest_selenium_automated_testing.html")
-time.sleep(10)
+time.sleep(5)
